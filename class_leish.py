ac961f4f2222ed754dd68efbc7349237a2a347a9
CrisSotomayor <cristina@sotomayor.com.mx>
2020-05-24 18:05:52 -0500

    Uploaded class_leish, sim_leish, fixed details.

--- class_leish.py
+++ class_leish.py
@@ -1,207 +1,214 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Class implementation of Leishmania 
-
-# In[1]:
-
-
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.animation as animation
 import random
-import time
-from IPython import display
-import seaborn as sns
-
-
-# In[3]:
 
 
 class Leishmania(object):
     alive = []
-    
-    def __init__(self, coord):
+    alive_dict = dict()
+    #spacesize = 10
+
+    def __init__(self, coord, followers=None):
+        """coord = coordinate, followers will track macrophages following it"""
         self.coord = np.array(coord)
+        if followers is None:
+            self.followers = []
+
         Leishmania.alive.append(self)
-    
+
+
     def Movement_Leish(self):
-        """ The Leishmania will move when iterated, one step, this place will be 
-        randomly chosen from its first neighbours """
-        neighborhood = FirstMooreNeighborhood(self.coord)
+        """Leishmania will move when iterated, one step, this place will be
+        randomly chosen from its first neighbors """
+        neighborhood = FirstMooreNeighborhood(self)
         self.coord = random.choice(neighborhood)
         return self.coord
-    
 
-
-# In[5]:
+    def Sort():
+        """Sort Leishmania.alive into a dict according to the first coordenate of each leish"""
+        for i in range(Leishmania.spacesize):
+            Leishmania.alive_dict[i] = [leish for leish in Leishmania.alive if leish.coord[0] == i]
+        return
 
 
 class Macrophage(object):
-    alive = [] #keep track of macrophages alive 
-    
-    def __init__(self, coord, infected, name, time, leish = None):
-        """coord = coordinate, infected := True if infected/False if healthy (boolean), 
-        name := type of macrophage, time:= time alive/when infected time infected """
+    alive = [] # Keep track of alive macrophages
+    infected = [] # Keep track of infected macrophages
+
+    def __init__(self, coord, infected=False, name=None, time=0, leish=None):
+        """coord = coordinate, infected := True if infected/False if healthy (boolean),
+        name := type of macrophage, time:= time alive/time infected """
         self.coord = np.array(coord)
-        self.infected = bool(infected) ##Iniciar como False (?)
-        self.name = name
-        self.time = int(time)
-        if leish is None: 
-            self.leish = []    
         Macrophage.alive.append(self)
-        
+        self.infected = infected
+        self.time = time
+        self.leish = leish
+
     def print_info(self):
-        #prints the information about a macro
-        print('Type: {}, Infected: {}, Coordinates: {}, Tiempo: {}, Chasing Leish: {}'
-              .format(self.name, self.infected, self.coord, self.time, self.leish))
-    
+        """"Prints macrophage information. """
+        print(f'Type: {self.name}, Infected: {self.infected},'
+              f'Coordinates: {self.coord}, Time: {self.time},'
+              f'Chasing Leish: {self.leish}')
+
     def Movement_Macro(self):
-        """The Macrophage will move when iterated, two steps, 
-        this place will be randomly chosen from its second neighbours"""
-        neighborhood = SecondMooreNeighborhood(self.coord)
-        step = random.choice(neighborhood) 
+        """The Macrophage will move when iterated, two steps,
+        this place will be randomly chosen from its second neighbors"""
+        neighborhood = SecondMooreNeighborhood(self)
+        step = random.choice(neighborhood)
         self.coord = step
         return self.coord
-    
+
     def Search_Leish(self):
-        '''Scan Moore neighborhood with range 3, if Leish found, track it'''
+        """Scan Moore neighborhood with range 3, if Leish found, track it"""
         neighborhood = []
         coord = self.coord
+        N = self.spacesize
+
+        # Get third Moore neighborhood, exclude own coordinate
         for i in range(-3,4):
             for j in range(-3,4):
                 if i!=0 or j!=0:
-                    neighborhood.append(((i+coord[0])%N,(j+coord[1])%N))
-        neighborhood = [x for x in vecindad if (x not in FirstMooreNeighborhood(coord))]
-        coordinates = [leish.coord for leish in Leishmania.alive]
-        #Si crece demasiado el número de leishmanias/macros, puede ser que generar la lista cada vez sea muy poco eficiente
-        #Podemos cambiar la función para que se aplique sobre la lista de macros y solo se genere una vez, no sé si Search_Leish(Macrophages.alive) puede estar dentro de la clase
-        for n, coor in enumerate(coordinates):
-            if coor in neighborhood: #no es realmente al azar la eleccion 
-                self.leish = Leishmania.alive[n]    
-                return self.leish.coord
-            
-    def Pursuit(self): 
-        '''For Macrophage tracking Leish, take a step towards Leish coordinate '''
-        neighborhood = SecondMooreNeighborhood(self.coord)
+                    neighborhood.append(np.array(((i+coord[0])%N,(j+coord[1])%N)))
+
+        # To reduce running time, look only in corresponding Moore neighborhood cols
+        interval = [(coord[0]+i)%N for i in range(-3,4)]
+        for i in interval:
+            for leish in Leishmania.alive_dict[i]:
+                if any(np.array_equal(leish.coord, x) for x in neighborhood):
+                    self.leish = leish
+                    self.leish.followers.append(self)
+                    return
+
+    def Chase(self):
+        """Take a step towards Leish being chased. """
+        neighborhood = SecondMooreNeighborhood(self)
         min_distance = Distance(self.leish.coord, neighborhood[0])
         closest = neighborhood[0]
-        for coor in enumerate(neighborhood): 
+        for coor in neighborhood:
             distance = Distance(self.leish.coord, coor)
             if distance < min_distance:
                 min_distance = distance
                 closest = coor
-        #move macrophage
-        self.coord = closest
+        return closest
+
+    def Pursuit(self, p):
+        '''For Macrophage tracking Leish, take a step towards Leish coordinate.
+        If Leish is reached, macrophage gets infected with probability p. '''
+
+        self.coord = self.Chase() # Move macrophage
+        # If leish in self.coord or first Moore neighborhood, phagocyte it
+        search_area = FirstMooreNeighborhood(self)
+        search_area.append(self.coord)
+        if any(np.array_equal(self.leish.coord, x) for x in search_area):
+            leish_to_delete = self.leish # Store leish to be deleted
+            # Alert followers of death, followers stop tracking it
+            for macro in self.leish.followers:
+                macro.leish = None
+            # Remove from alive list and dict
+            Leishmania.alive.remove(leish_to_delete)
+            Leishmania.alive_dict[leish_to_delete.coord[0]].remove(leish_to_delete)
+
+            if random.random() < p: # Infected with probability p
+                self.infected = True
+                Macrophage.infected.append(self)
+                self.time = 0
         return self.coord
-    
+
     def Leish_Reproduction(self):
         '''Infected Macrophage dies, releasing new Leish'''
-        neighborhood = SecondMooreNeighborhood(self.coord)
+        neighborhood = SecondMooreNeighborhood(self)
         for coor in neighborhood:
-            #!!! no existe esto aun 
-            ##Leishmania.alive.append(coor)
-            ## crear las leishmanias como objetos!
-            ##Sin el espacio en sí, aqui podemos meter fácilmente la cantidad de leishmanias, ya no importa si se traslapan 
-        #Macrophage dies
-        Macrophages.alive.remove(self)
+            Leishmania(coor)
+        Macrophage.alive.remove(self)
+        Macrophage.infected.remove(self)
         return
-    
-
-
-# In[1]:
 
 
 #Neighborhood functions
-def FirstMooreNeighborhood(coordinate): 
-    '''Moore neighborhood of coordinate with range 1, excluding coordinate '''
+def FirstMooreNeighborhood(agent):
+    '''Moore neighborhood of object with range 1, excluding coordinate '''
     neighborhood = []
+    coordinate = agent.coord
+    N = agent.spacesize
     for i in range(-1,2):
         for j in range(-1,2):
             if i!=0 or j!=0:
-                neighborhood.append(((i+coordinate[0])%N,(j+coordinate[1])%N))
+                neighborhood.append(np.array(((i+coordinate[0])%N,(j+coordinate[1])%N)))
     return neighborhood
 
 
-def SecondMooreNeighborhood(coordinate):
+def SecondMooreNeighborhood(agent):
     '''Moore neighborhood of coordinate with range 2, excluding neighborhood with range 1'''
-    neighborhood = []
-    for i in range(-2,3):
-        for j in range(-2,3):
-            if i!=0 or j!=0:
-                neighborhood.append(((i+coordinate[0])%N,(j+coordinate[1])%N))
-    neighborhood = [x for x in neighborhood if (x not in FirstMooreNeighborhood(coordinate))]
+    coordinate = agent.coord
+    N = agent.spacesize
+    auxi = [[-2,-2],[-2,-1],[-2,0],[-2,1],[-2,2], # improve this
+            [-1,-2],[-1,2],[0,-2],[0,2],[1,-2],[1,2],
+            [2,-2],[2,-1],[2,0],[2,1],[2, 2]]
+    neighborhood = [np.array(np.array(((i[0]+coordinate[0])%N,(i[1]+coordinate[1])%N))) for i in auxi]
+
     return neighborhood
 
+#Other functions
+def Recruitment(coordinates, num_macro, recruit_rate):
+    '''Create new macrophages, creates recruit_rate percentage of original population'''
+    new_macros = int(num_macro*recruit_rate)
+    new_coords = random.sample(coordinates, new_macros)
 
-# In[ ]:
+    for coor in new_coords:
+        Macrophage(coor)
+    return
 
 
-#Drawing functions        
 def DrawSpace():
-    '''Plot Space matrix'''
+    """Plot Space matrix. """
+    size = Macrophage.spacesize
+
     Space = np.zeros((size, size))
     for leish in Leishmania.alive:
-        (x,y) = leish.coord
+        x = leish.coord[0]
+        y = leish.coord[1]
         Space[x][y] = 1
-    for macro in Macrophages.alive:
-        neighborhood = FirstMooreNeighborhood(macro.coord)
+    for macro in Macrophage.alive:
+        neighborhood = FirstMooreNeighborhood(macro)
         neighborhood.append(macro.coord)
         if macro.infected == False:
-            for (x,y) in neighborhood:
-                Space[x][y] = 2
+            for co in neighborhood:
+                Space[co[0]][co[1]] = 2
         else:
-            for (x,y) in neighborhood:
-                Space[x][y] = 3
-        
+            for co in neighborhood:
+                Space[co[0]][co[1]] = 3
+
     f,ax = plt.subplots(1,1,figsize=(3,3),dpi=120)
     ax.imshow(Space,vmin=0,vmax = 3,cmap='jet')
     plt.show()
     plt.close()
-    
+
     return Space
 
 
-# In[ ]:
+def GraphPopulations(populations, p, num_leish, num_macro):
+    plt.figure()
+    plt.title('Population Evolution (p = {}, M = {}, L = {})'.format(p, populations['Macrophages'][0],
+                                                                        populations['Leishmanias'][0]))
+    plt.ylabel('Population')
+    plt.xlabel('Days')
+
+    t = np.linspace(0, len(populations['Leishmanias']) - 1, len(populations['Leishmanias']))
+
+    plt.plot(t, populations['Leishmanias'], label = 'Leishmanias')
+    plt.plot(t, populations['Healthy'], label = 'Healthy')
+    plt.plot(t, populations['Infected'], label = 'Infected')
+    plt.plot(t, populations['Macrophages'], label = 'Macrophages')
+    plt.legend(loc = 'upper left')
+
+    #plt.savefig('p'+ str(p) + 'M'+ str(num_macro)+ 'L'+ str(num_leish)+ '.png')
+    plt.show()
+    plt.close()
+
+    return None
 
 
-#Other functions
 def Distance(a,b):
     '''Distance from a to b, rounded to 1 decimal'''
     dist = ((a[0]-b[0])**2+(a[1]-b[1])**2)**(1/2)
     return round(dist,1)
-
-
-# In[ ]:
-
-
-class Leuco(Macrophage):
-    pass
-
-
-# Notas:
-# <ol>
-#     <li>regresar error si algo no se puede (ej. pedir leish_reproduction a un macro no infectado)
-#     <li>no sé cómo usar las listas de leish/macro, parece mala idea usarlas en las clases si las listas se definen fuera de ellas, no sé qué tan bien es definirlas dentro de las clases (weakrefs?) 
-#     <li>donde meter info sobre días/cuantas iteraciones hacen un día
-#     <li>que macrophages y leishmania sean subclases? que la clase principal sea como 'agents' o algo, puede facilitar llevar la cuenta de las coordenadas dentro de las clases
-#     <li>se puede obtener lista de coord facilmente? Algo como [i.coord for i in Macrophage.alive]? Sí, eso sirve
-                #si lo corres muchas veces, se guardan cosas repetidas, hay que vaciar la lista cuando corramos más simulaciones
-#       #en el tiempo, llevaremos la cuenta al moverlo? o definimos una función que sea como "iterar" y lo cambiamos ahi?
-
- #<ol/>
-#         
-#         
-# Por definir:
-# <ol>
-#     <li>def initial_population 
-         # size (tamaño inicial )
-#     <li>coordinates
-#    
-# <ol/>
-
-# In[ ]:
-
-
-
-
---
+++ sim_leish.py
@@ -0,0 +1,116 @@
+import sys
+import class_leish as cll
+import numpy as np
+import random
+import time
+
+
+def simulation(num_leish, num_macro, p, recruit_rate, days=70, steps=96,
+                len_infection=28, size=100):
+    """Run simulation.
+
+    Parameters:
+        num_leish (int): initial leishmania population
+        num_macro (int): initial macrophage population
+        p (float): probability that leish will infect macrophage following
+                   phagocytosis
+        recruit_rate (float): percent of initial population of macrophages that
+                              get recruited
+        days (int): number of days that simulation will run, default is 70 days
+                    (10 weeks)
+        steps (int): number of "steps" in a day, default is 96 for 15 minute steps
+        len_infection (int): days infected before releasing leish
+        size (int): space considered will be grid size x size
+
+
+        """
+    start_time = time.time()
+    #Set size as attribute for Leishmania and Macrophage
+    cll.Leishmania.spacesize = size
+    cll.Macrophage.spacesize = size
+    #Emmpty alive lists and dict
+    cll.Leishmania.alive = []
+    cll.Leishmania.alive_dict = {i:[] for i in range(size)}
+    cll.Macrophage.alive = []
+    # Data for graphs
+    populations = {'Macrophages':[], 'Healthy':[], 'Infected':[], 'Leishmanias':[]}
+    # Keep track of recruitment days and if recruitment is active
+    recruit_days = 0
+    recruitment = False
+
+    # Save initial data for graph
+    populations['Macrophages'].append(num_macro)
+    populations['Healthy'].append(num_macro)
+    populations['Infected'].append(0) # Initially 0 are infected
+    populations['Leishmanias'].append(num_leish)
+
+    iters = int(days*steps) # Iterations the model will perform
+
+    coordinates = []
+    for i in range(size):
+        for j in range(size):
+            coordinates.append((i,j))
+
+    # Create leish and macros, distributed randomly in space
+    coor_leish = random.sample(coordinates, num_leish)
+    for coor in coor_leish:
+        cll.Leishmania(coor)
+
+    coor_macro = random.sample(coordinates, num_macro)
+    for coor in coor_macro:
+        cll.Macrophage(coor)
+
+    for i in range(iters):
+        # Iterate over macros
+        for macro in cll.Macrophage.alive:
+            macro.time += 1 # Increase time alive or time infected
+
+            if macro.infected == True:
+                # If length of infection reached, leish reproduces, else macro moves
+                if macro.time == len_infection*steps:
+                    macro.Leish_Reproduction()
+                    cll.Leishmania.Sort()
+                else:
+                    macro.Movement_Macro()
+            else: # Not infected
+                if macro.leish: # If tracking, pursuit
+                    macro.Pursuit(p)
+                else: # If not tracking, search
+                    macro.Search_Leish()
+                    if macro.leish: # If now tracking, pursuit
+                        macro.Pursuit(p)
+                    else:
+                        macro.Movement_Macro()
+
+        for leish in cll.Leishmania.alive:
+            leish.Movement_Leish()
+
+        cll.Leishmania.Sort() # Sort leish according to new coord
+
+        # If there are more leishmanias than the original amount, assume
+        # infection is spreading and begin recruitment
+        if len(cll.Leishmania.alive) > num_leish:
+            if recruit_days < 7:
+                recruitment = True
+        # Recruit once each day
+        if i%steps == 0 and recruitment == True:
+            recruit_days += 1
+            cll.Recruitment(coordinates, num_macro, recruit_rate)
+        # Stop after a week
+        if recruit_days > 7:
+            recruitment = False
+
+
+        if (i+1)%steps == 0: # Save data at the end of each day (i+1 to save last iteration)
+            populations['Macrophages'].append(len(cll.Macrophage.alive))
+            populations['Healthy'].append(len(cll.Macrophage.alive) - len(cll.Macrophage.infected))
+            populations['Infected'].append(len(cll.Macrophage.infected))
+            populations['Leishmanias'].append(len(cll.Leishmania.alive))
+            cll.DrawSpace()
+
+    # Once simulation is done, create graph, print final amounts and time run
+    cll.GraphPopulations(populations, p, num_leish, num_macro)
+    print('Leishmanias = {}'.format(len(cll.Leishmania.alive)), '\nMacrophages = {}'.format(len(cll.Macrophage.alive)))
+    print('{} days'.format(round((i+1)/steps, 2)))
+    end_time = time.time()
+    print(round(end_time-start_time, 2), ' seconds')
