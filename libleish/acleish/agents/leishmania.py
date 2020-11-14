1d8eca6d67b1a6729d9fd1e184442ff2fd66f6c3
Ulises Rayón <uliarp@ciencias.unam.mx>
2020-11-14 14:59:36 -0600

    all

--- libleish/acleish/agents/leishmania.py
+++ libleish/acleish/agents/leishmania.py
@@ -3,29 +3,27 @@ import numpy as np
 import random
 import csv
 
+from auxiliar_func import *
 
 class Leishmania(object):
     alive = []
-    alive_dict = dict()
-    #spacesize = 10
+    alive_dict = {}
 
-    def __init__(self, coord, followers=None):
+    def __init__(self, coord, spacesize=10, followers=None) -> None:
         """coord = coordinate, followers will track macrophages following it"""
+        self.spacesize=spacesize
         self.coord = np.array(coord)
         if followers is None:
             self.followers = []
+        self.alive.append(self)
 
-        Leishmania.alive.append(self)
-
-    def Movement_Leish(self):
+    def move(self) -> None:
         """Leishmania will move when iterated, one step, this place will be
         randomly chosen from its first neighbors """
-        neighborhood = FirstMooreNeighborhood(self)
-        self.coord = random.choice(neighborhood)
-        return self.coord
+        self.coord = random.choice(neighborhood(self))
+        #return self.coord <------------ Let's see how this works
 
-    def Sort():
+    def sort(self) -> None:
         """Sort Leishmania.alive into a dict according to the first coordenate of each leish"""
-        for i in range(Leishmania.spacesize):
-            Leishmania.alive_dict[i] = [leish for leish in Leishmania.alive if leish.coord[0] == i]
-        return
+        for i in range(self.spacesize):
+            self.alive_dict[i] = [leish for leish in self.alive if leish.coord[0] == i]
